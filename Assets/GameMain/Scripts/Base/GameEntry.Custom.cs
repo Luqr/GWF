9c9beb2da1ee5562b93dcff630a5c6920d757b10
R-LUQIANRUI\Administrator <Kanelu@126.com>
2022-02-25 11:05:49 +0800

    基本配置提交   自动转化表枚举&CS文件

---
+++ Assets/GameMain/Scripts/Base/GameEntry.Custom.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityGameFramework.Runtime;
+
+namespace Flower
+{
+    /// <summary>
+    /// 游戏入口。
+    /// </summary>
+    public partial class GameEntry : MonoBehaviour
+    {
+        private static void InitCustomComponents()
+        {
+        }
+
+    }
+}
